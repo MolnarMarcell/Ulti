03ef9daa4061ac29218303d133f6b3b736e3cff4
MolnarMarcell <marci20070208@gmail.com>
2026-01-22 15:05:09 +0100

    Add project files.

---
+++ Jatekosok.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulti
+{
+    internal class Jatekosok
+    {
+
+        public string elsojatekos;
+        public string masodikjatekos;
+        public string harmadikjatekos;
+        public string negyedikjatekos;
+        public Jatekosok(string elso, string masodik, string harmadik, string negyedik)
+        {
+            elsojatekos = elso;
+            masodikjatekos = masodik;
+            harmadikjatekos = harmadik;
+            negyedikjatekos = negyedik;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ulti
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+        List<Jatekosok> jatekosoks = new List<Jatekosok>();
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+
+
+        private void OKGOMB_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(elso.Text) || string.IsNullOrEmpty(masodik.Text) || string.IsNullOrEmpty(harmadik.Text) || string.IsNullOrEmpty(negyedik.Text))
+            {
+                MessageBox.Show("Kérem töltse ki az összes mezőt!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+                Jatek jatekwindow = new Jatek();
+
+            //
+                Jatekosok ujjatekos = new Jatekosok(elso.Text, masodik.Text, harmadik.Text, negyedik.Text);
+                jatekosoks.Add(ujjatekos);
+
+                foreach (var sor in jatekosoks)
+                {
+                    vissza.Content = $"{sor.elsojatekos}, {sor.masodikjatekos}, {sor.harmadikjatekos}, {sor.negyedikjatekos}";
+                }
+
+                Application.Current.MainWindow = jatekwindow;
+                jatekwindow.Show();
+
+                this.Close();
+        }
+    }
+}
